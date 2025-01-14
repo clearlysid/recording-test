a54fbc35f794b108ccacf0377d16f1e72bf4aed4
Sahil Yeole <sahilyeole93@gmail.com>
2025-01-14 12:51:35 +0530

    test: crop area
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,55 @@
 mod encoder;
 
 use anyhow::Error;
-use std::sync::mpsc;
-use std::path::Path;
+use crabgrab::util::{CropArea, Point, Rect, Size};
 use encoder::{Encoder, VideoEncoder};
 use pollster::FutureExt;
+use std::path::Path;
+use std::sync::mpsc;
 
 use crabgrab::capturable_content::{CapturableContent, CapturableContentFilter};
-use crabgrab::capture_stream::{CaptureConfig, CaptureStream, StreamEvent, CapturePixelFormat};
+use crabgrab::capture_stream::{CaptureConfig, CapturePixelFormat, CaptureStream, StreamEvent};
 
 // Variables to configure the stream
 // Encoder configs are in the ./encoder folder
 const STREAM_PX_FMT: CapturePixelFormat = CapturePixelFormat::Bgra8888;
-const SCALE_FACTOR: f64 = 1.0; // NOTE: on macbooks this can be 2.0
+const SCALE_FACTOR: f64 = 2.0; // NOTE: on macbooks this can be 2.0
 const OUTPUT_FILE: &str = "./video.mp4";
 
 fn main() -> Result<(), Error> {
-
     // MARK: Configure Stream
     let content = CapturableContent::new(CapturableContentFilter::DISPLAYS).block_on()?;
-    let display = content.displays().next().ok_or(Error::msg("No displays found"))?;
+    let display = content
+        .displays()
+        .next()
+        .ok_or(Error::msg("No displays found"))?;
 
     let size = display.rect().scaled(SCALE_FACTOR).size; // Hardcoded to 2 (as scale factor)
-    let height = size.height;
-    let width = size.width;
+    let mut height = size.height;
+    let mut width = size.width;
+
+    let crop = CropArea {
+        origin: Point {
+            x: 1116.0,
+            y: 309.0,
+        },
+        size: Size {
+            width: 496.0,
+            height: 440.0,
+        },
+        scale_factor: Some(SCALE_FACTOR),
+    };
+
+    width = crop.size.width * SCALE_FACTOR;
+    height = crop.size.height * SCALE_FACTOR;
 
     let stream_cfg = CaptureConfig::with_display(display, STREAM_PX_FMT, None)
         .with_color_space_name("kCGColorSpaceSRGB".to_string())
+        .with_crop_area(Some(crop))
         .with_output_size(size);
 
     let stream_token =
-    CaptureStream::test_access(false).ok_or(Error::msg("Failed to get access token"))?;
+        CaptureStream::test_access(false).ok_or(Error::msg("Failed to get access token"))?;
 
     // MARK: Configure Encoder
     let output = Path::new(OUTPUT_FILE);
@@ -52,7 +71,7 @@ fn main() -> Result<(), Error> {
             StreamEvent::Video(frame) => {
                 println!("got new frame");
                 tx.send(Some(frame)).expect("couldn't send frame");
-            },
+            }
             StreamEvent::End => match tx.send(None) {
                 Ok(_) => {}
                 Err(e) => {
@@ -63,7 +82,6 @@ fn main() -> Result<(), Error> {
         },
         Err(e) => eprintln!("Error: {}", e),
     })?;
-
 
     // MARK: Record for 3 seconds, then stop
     std::thread::sleep(std::time::Duration::from_secs(3));
